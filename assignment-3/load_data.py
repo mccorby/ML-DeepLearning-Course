abc1479a657b1a80bf3f6be74a6f7a6742530d7a
Jose Corbacho <JoseAntonio.Corbacho@sky.uk>
2017-02-15 16:39:56 +0000

    Assigment 3. Added Regularization l2

---
+++ assignment-3/load_data.py
@@ -0,0 +1,17 @@
+import os
+from six.moves import cPickle as pickle
+
+
+def load_data(data_root):
+    pickle_file = os.path.join(data_root, 'notMNIST.pickle')
+
+    with open(pickle_file, 'rb') as f:
+        save = pickle.load(f)
+        train_dataset = save['train_dataset']
+        train_labels = save['train_labels']
+        valid_dataset = save['valid_dataset']
+        valid_labels = save['valid_labels']
+        test_dataset = save['test_dataset']
+        test_labels = save['test_labels']
+        del save  # hint to help gc free up memory
+        return train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels
